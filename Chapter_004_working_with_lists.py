73a54d2102d5d3a17ede4f4d443805ad66d1ad9b
researchmonkey <research_monkey@live.com>
2019-01-19 12:02:22 -0600

    Initial commit

---
+++ Chapter_004_working_with_lists.py
@@ -0,0 +1,20 @@
+# data=['manufacturer_name','unii','product_type','spl_set_id','route','generic_name','upc','brand_name','product_ndc',]
+# print(data)
+# for data in data:
+#     print(data.title(),"is an item in a list")
+# stopped at Making a numerical list
+
+# for value in range(1,5):
+#     print(value)
+
+# number = list(range(1,8))
+# print(number)
+#
+# even_number = list(range(3,30,3))
+# print(even_number)
+
+squares =[]
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
