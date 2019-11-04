051e694ee1ffebd260af2e41a720b2e26906b446
research_monkey <research_monkey@live.com>
2019-11-03 19:36:58 -0600

    added chapet 8 plus the source PDF

--- Chapter_005_if_statements.py
+++ Chapter_005_if_statements.py
@@ -1,4 +1,4 @@
-cars =["audi","bmw","subaru","toyota","ford"]
+cars = ["audi", "bmw", "subaru", "toyota"]
 for car in cars:
     if car == "bmw":
         print(car.upper())
@@ -7,6 +7,7 @@ for car in cars:
 if "ford" not in cars:
     cars.append("ford")
     print(cars)
+    print("Added Ford")
 else:
     print("Its there already!")
 
@@ -16,15 +17,15 @@ driver_max_age = 90
 my_age = 17
 
 if (my_age >= driver_min_age) and (my_age <= driver_max_age):
-    print("At", my_age,"your able to drive in this state.")
+    print("At", my_age, "your able to drive in this state.")
 else:
     if my_age <= driver_min_age:
-        print("At", my_age,"your to young to drive in this state!")
+        print("At", my_age, "your to young to drive in this state!")
     if my_age >= driver_max_age:
-        print("At", my_age,"your to old to drive in this state!")
+        print("At", my_age, "your to old to drive in this state!")
 
 min_age = 4
-jr_age = range(4,19)
+jr_age = range(4, 19)
 
 your_age = 13
 
@@ -35,7 +36,6 @@ elif your_age <= min_age:
 else:
     print("Your admission fee if $10.00, Enjoy the show!")
 
-
 if your_age <= 4:
     price = 0
 elif your_age < 18:
@@ -43,23 +43,23 @@ elif your_age < 18:
 else:
     price = 10
 
-print("Your admission fee is $"+ str(price)+".00.")
-in_stock =['mushrooms', 'extra cheese',"meat", 'green peppers']
-out_stock =["olives"]
+print("Your admission fee is $" + str(price) + ".00.")
+in_stock = ['mushrooms', 'extra cheese', "meat", 'green peppers']
+out_stock = ["olives"]
 
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese',"olives"]
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese', "olives"]
 
 for requested_topping in requested_toppings:
     if requested_topping in out_stock:
-        print("Sorry, we are out of "+requested_topping,"right now.")
+        print("Sorry, we are out of " + requested_topping, "right now.")
     else:
-        print("Adding",requested_topping+".")
+        print("Adding", requested_topping + ".")
 if requested_toppings == []:
     print("Are you sure you want a plain pizza?")
 else:
     print("\nYour Pizza is being made.")
 
-numbers = range(1,10)
+numbers = range(1, 10)
 for number in numbers:
     if number == 1:
         num_end = "st"
@@ -69,10 +69,9 @@ for number in numbers:
         num_end = "rd"
     else:
         num_end = "th"
-    print(str(number)+num_end)
+    print(str(number) + num_end)
 
-
-alien_colors = ["green", "yellow","red"]
+alien_colors = ["green", "yellow", "red"]
 player_points = 0
 for alien in alien_colors:
     if alien == "red":
@@ -85,8 +84,7 @@ for alien in alien_colors:
         player_points += 5
         print("player earned 5 points")
 
-print("The player has",player_points,"points")
-
+print("The player has", player_points, "points")
 
 your_age = 77
 
