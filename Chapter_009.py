9eaacd7c00a39c35feb1dc7e031c998189cb869f
ResearchMonkey <Research_monkey@live.com>
2019-11-27 18:24:48 -0600

    Update Chapter_009.py
    Added car class

--- Chapter_009.py
+++ Chapter_009.py
@@ -43,3 +43,39 @@ class Restaurant():
 # print(my_res.open_restaurant())
 
 # stopped at Working with Classes and Instances on page 167
+
+
+class Car():
+    """A simple attempt to to rep a car"""
+
+    def __init__(self, make, model, year):
+        """int attributes to describe a car"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name_one(self):
+        """"reteuns a formmated script of the car"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print the millage"""
+        print("This car has {} miles on it.".format(self.odometer_reading))
+        # print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self):
+        """update the mialage"""
+
+
+my_first = Car("Pontiac", 'Grand AM', 2001)
+
+
+print(my_first.get_descriptive_name())
+my_first.read_odometer()
